91b892afa9188d721791934c156884b72cd4178f
Alexey <ololmkbc@gmail.com>
2020-11-20 18:47:18 +0200

    Few changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {Text, View} from 'react-native';
+import {Provider, useDispatch} from 'react-redux';
+import CoinList from './components/CoinList';
+import Loading from './components/Loading';
+import store from './redux/Store';
+
 const App = () => {
   const isHermes = () => !!global.HermesInternal;
   return (
-    <>
-      <Text>Hermes Engine: {isHermes().toString()}</Text>
-    </>
+    <Provider store={store}>
+      {/* <Text>Hermes Engine: {isHermes().toString()}</Text>*/}
+      <CoinList />
+    </Provider>
   );
 };
-
 export default App;
+/*
+  <Image
+    source={{
+      uri: `https://www.cryptocompare.com${item.CoinInfo.ImageUrl}`,
+    }}
+    style={{width: 50, height: 50}}
+  />
+  <Text>{item.CoinInfo.FullName}</Text> 
+*/
