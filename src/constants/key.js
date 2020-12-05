416a8292096717d659f723d9d3668f0fbe43595f
Exxodia666 <ololmkbc@gmail.com>
2020-12-05 02:41:26 +0200

    New links

--- src/constants/key.js
+++ src/constants/key.js
@@ -1,4 +1,5 @@
 export const key = '4768f7b6aeeac511bce1b24a8362e2c08214307f3b7ccb7146be6237ed33f5b1';
-//TODO multiple currency
-export const url = `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=`;
-export const apikey = '&api_key='
+
+export const coins_url = (currency) => `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=${currency}&api_key=${key}`;
+export const daily_coins_url = (currency, crypto_currency) => `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${crypto_currency}&tsym=${currency}&limit=6&api_key=${key}`;
+
