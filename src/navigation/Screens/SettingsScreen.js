50a4f4690dac9b35dbb82919d8aad9d2c542f46a
Exxodia666 <ololmkbc@gmail.com>
2020-12-05 02:42:52 +0200

    Update settings

--- src/navigation/Screens/SettingsScreen.js
+++ src/navigation/Screens/SettingsScreen.js
@@ -64,16 +64,6 @@ const SettingsScreen = () => {
           Alert.alert('Saved!');
         }}
       />
-      {/*
-      <Picker
-        selectedValue={selectedValue}
-        style={{height: 50, width: 150}}
-        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
-      </Picker>
-
-      {/**/}
     </View>
   );
 };
--- src/redux/reducers/settings/settings.js
+++ src/redux/reducers/settings/settings.js
@@ -1,13 +1,11 @@
-import {SAVE_SETTINGS} from '../actions/settings';
-
+import { SAVE_SETTINGS } from "../../actions/settings";
 const initialState = {
   currency: 'UAH',
 };
-
 export default (state = initialState, action) => {
   switch (action.type) {
     case SAVE_SETTINGS:
-      return {...state, currency: action.data};
+      return { ...state, currency: action.data };
     default:
       return state;
   }
